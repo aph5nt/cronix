5b7aa715521d2807aeadcee6b9d37e96008ef784
Kamil Wojciechowski <kamil@socialcee.com>
2015-03-07 18:21:02 +0100

    initial commit

---
+++ src/Cronix/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Cronix")>]
+[<assembly: AssemblyProductAttribute("Cronix")>]
+[<assembly: AssemblyDescriptionAttribute("F# / C# Cron Service")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/Cronix.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Cronix.Tests
+
+open Cronix
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
