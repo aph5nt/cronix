80aab44203ee66f50d5f0598105dfb3b1ee67fed
Kamil Wojciechowski <kamil@socialcee.com>
2015-03-28 16:59:04 +0100

    added F# sample

---
+++ docs/samples/FSharpSample/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open Cronix
+open Chessie.ErrorHandling
+open System.Threading
+
+let sampleJob (token : CancellationToken) = 
+      printf "callback executed at (UTC) %s\n" <| DateTime.UtcNow.ToString()
+      Thread.Sleep 100
+ 
+[<EntryPoint>]
+let main argv = 
+ 
+    let startupHandler = 
+        new StartupHandler(
+            fun(scheduler) ->
+                        scheduler.Schedule "scheduled job" "* * * * *" <| Callback( sampleJob ) |> ignore
+            )
+
+    let result = BootStrapper.InitService(Some(argv), Some(startupHandler))
+    match result with
+    | Ok (state, msgs) -> printfn "%s" state
+    | Fail msgs -> msgs |> List.iter(fun(s) ->  printfn "%s" s)
+    0 // return an integer exit code
---
+++ docs/samples/FSharpSample/Startup.fsx
@@ -0,0 +1,25 @@
+﻿#if INTERACTIVE
+#I "."
+#I "bin\Debug"
+#I "bin\Release"
+#r "Cronix.dll"
+#r "FSharpSample.exe"
+#r "Chessie.dll"
+#endif
+
+namespace Cronix.Startup
+
+module RunAtStartup =
+    
+    open System.Threading
+    open System
+    open Cronix
+    open Messages
+
+    let sampleJob (token : CancellationToken) = 
+      printf "callback executed at (UTC) %s\n" <| DateTime.UtcNow.ToString()
+      Thread.Sleep 100
+
+    let start (scheduler : IScheduleManager) =
+        scheduler.Schedule "job1" "* * * * *" <| Callback(sampleJob) |> ignore       
+        ()
