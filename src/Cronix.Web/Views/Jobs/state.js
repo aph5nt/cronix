f701a336a72428fc92bf906106135876ed227f24
Kamil Wojciechowski <kamil@socialcee.com>
2015-04-06 19:30:42 +0200

    webui

---
+++ src/Cronix.Web/Views/Jobs/state.js
@@ -0,0 +1,34 @@
+﻿window.cronix = window.cronix || {};
+
+$(document).ready(function () {
+    
+    cronix.stateItem = {
+        name: ko.observable(''),
+        cronExpr: ko.observable(''),
+        stateType: ko.observable(''),
+        nextOccurance: ko.observable('')
+    };
+
+    cronix.StateViewModel = function() {
+       
+        var state = ko.observableArray([]),
+        hasAny = ko.computed(function () {
+            console.log(state.length);
+            return state.length > 0;
+        }),
+        initialize = function() {};
+
+        return {
+            state: state,
+            hasAny: hasAny,
+            initialize: initialize
+        };
+    }
+
+    var vm = new cronix.StateViewModel();
+    vm.initialize();
+    ko.applyBindings(vm);
+   
+});
+ 
+ 
