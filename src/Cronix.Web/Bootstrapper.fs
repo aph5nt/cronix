df0ae53a8d24170e7b6fad5ab4b333020b6491a1
Kamil Wojciechowski <kamil@socialcee.com>
2015-04-09 17:28:12 +0200

    nancy under owin

--- src/Cronix.Web/Bootstrapper.fs
+++ src/Cronix.Web/Bootstrapper.fs
@@ -1,9 +1,12 @@
 ﻿namespace Cronix.Web
 
 open Nancy
+open Nancy.TinyIoc
+open Nancy.Bootstrapper
 
 type Bootstrapper() =
     inherit DefaultNancyBootstrapper()
-    // The bootstrapper enables you to reconfigure the composition of the framework,
-    // by overriding the various methods and properties.
-    // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+    override x.ApplicationStartup(container :TinyIoCContainer,  pipelines : IPipelines) =
+        ()
+   
--- src/Cronix.Web/Program.fs
+++ src/Cronix.Web/Program.fs
@@ -1,16 +1,35 @@
-﻿module Nancy.Templates.CSharp.SelfHostWithRazor.Program
+﻿namespace Cronix.Web
+
 
 open System
-open Nancy.Hosting.Self
+open Nancy
+open Microsoft.Owin.Hosting
+ 
+ 
 
-[<EntryPoint>]
-let main args =
-    let uri = Uri("http://localhost:3579")
+module EntryPoint = 
 
-    use host = new NancyHost(uri)
-    host.Start()
 
-    Console.WriteLine("Your application is running on " + uri.AbsoluteUri)
-    Console.WriteLine("Press any [Enter] to close the host.")
-    Console.ReadLine() |> ignore
-    0
+
+
+    [<EntryPoint>]
+    let main args =
+        let uri = Uri("http://localhost:8085")
+
+        // Owin configuration
+        let options = new StartOptions()
+        options.Port <- new Nullable<int>(8085)
+         
+        // Nancy configuration
+        let nancyOptions = new Nancy.Owin.NancyOptions()
+        nancyOptions.Bootstrapper <- new Bootstrapper()
+
+        WebApp.Start(options,
+            fun(app : Owin.IAppBuilder) -> (
+                                             app.Use(Nancy.Owin.NancyMiddleware.UseNancy(nancyOptions)) |> ignore
+            )) |> ignore
+ 
+        Console.WriteLine("Your application is running on " + uri.AbsoluteUri)
+        Console.WriteLine("Press any [Enter] to close the host.")
+        Console.ReadLine() |> ignore
+        0
